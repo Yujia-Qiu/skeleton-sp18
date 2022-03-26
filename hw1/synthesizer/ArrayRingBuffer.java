cdec5f9a8456a9cb1dc688bceb73026e305fddc0
yujiaqiu <yujia00.qiu@gmail.com>
2022-03-26 01:32:18 -0700

    Hw1

--- hw1/synthesizer/ArrayRingBuffer.java
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -1,10 +1,10 @@
-// TODO: Make sure to make this class a part of the synthesizer package
-// package <package name>;
+package synthesizer;// TODO: Make sure to make this class a part of the synthesizer package
+
 import java.util.Iterator;
 
 //TODO: Make sure to make this class and all of its methods public
 //TODO: Make sure to make this class extend AbstractBoundedQueue<t>
-public class ArrayRingBuffer<T>  {
+public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> {
     /* Index for the next dequeue or peek. */
     private int first;            // index for the next dequeue or peek
     /* Index for the next enqueue. */
@@ -21,6 +21,22 @@ public class ArrayRingBuffer<T>  {
         //       this.capacity should be set appropriately. Note that the local variable
         //       here shadows the field we inherit from AbstractBoundedQueue, so
         //       you'll need to use this.capacity to set the capacity.
+        first = 0;
+        last = 0;
+        fillCount = 0;
+        this.capacity = capacity;
+        rb = (T[]) new Object[capacity];
+
+    }
+
+    @Override
+    public int capacity() {
+        return capacity;
+    }
+
+    @Override
+    public int fillCount() {
+        return fillCount;
     }
 
     /**
@@ -30,6 +46,13 @@ public class ArrayRingBuffer<T>  {
      */
     public void enqueue(T x) {
         // TODO: Enqueue the item. Don't forget to increase fillCount and update last.
+        if(isFull()) {
+            throw new RuntimeException("Rinf buffer overflow");
+        }
+        rb[last] = x;
+        last = (last + 1 == capacity ? 0 : last + 1);
+        fillCount++;
+
     }
 
     /**
@@ -38,14 +61,23 @@ public class ArrayRingBuffer<T>  {
      * covered Monday.
      */
     public T dequeue() {
-        // TODO: Dequeue the first item. Don't forget to decrease fillCount and update 
+        // TODO: Dequeue the first item. Don't forget to decrease fillCount and update
+        if(isEmpty()) {
+            throw new RuntimeException("Rinf buffer overflow");
+        }
+        T res = rb[first];
+        rb[first] = null;
+        first = (first + 1 == capacity ? 0 : first + 1);
+        fillCount--;
+        return res;
     }
 
-    /**
-     * Return oldest item, but don't remove it.
-     */
     public T peek() {
         // TODO: Return the first item. None of your instance variables should change.
+        if(isEmpty()) {
+            throw new RuntimeException("Rinf buffer overflow");
+        }
+        return rb[first];
     }
 
     // TODO: When you get to part 5, implement the needed code to support iteration.
